778e1f6c3e5053e416115af3a1e37be9f250baa4
Markus Teich <markus.teich@stusta.mhn.de>
2018-12-01 02:33:01 +0100

    new key bindings

--- config.h
+++ config.h
@@ -37,17 +37,17 @@ static Bool npisrelative  = True;
 
 #define MODKEY Mod4Mask
 static Key keys[] = {
-	/* modifier             key            function     argument */
-	{ MODKEY,               XK_z,          focusonce,   { 0 } },
-	{ MODKEY,               XK_z,          spawn,       { 0 } },
+	/* modifier              key            function     argument */
+	{ MODKEY,                XK_z,          focusonce,   { 0 } },
+	{ MODKEY,                XK_z,          spawn,       { 0 } },
 
-	{ MODKEY,               XK_adiaeresis, rotate,      { .i = +1 } },
-	{ MODKEY,               XK_odiaeresis, rotate,      { .i = -1 } },
-	{ MODKEY|ShiftMask,     XK_odiaeresis, movetab,     { .i = -1 } },
-	{ MODKEY|ShiftMask,     XK_adiaeresis, movetab,     { .i = +1 } },
-	{ MODKEY,               XK_f,          rotate,      { .i = 0 } },
+	{ ControlMask,           XK_Tab,        rotate,      { .i = +1 } },
+	{ ControlMask|ShiftMask, XK_Tab,        rotate,      { .i = -1 } },
+	{ ControlMask|ShiftMask, XK_Page_Up,    movetab,     { .i = -1 } },
+	{ ControlMask|ShiftMask, XK_Page_Down,  movetab,     { .i = +1 } },
+	{ MODKEY,                XK_f,          rotate,      { .i = 0 } },
 
-	{ MODKEY,               XK_p,          spawn,       SETPROP("_TABBED_SELECT_TAB") },
+	{ MODKEY,                XK_p,          spawn,       SETPROP("_TABBED_SELECT_TAB") },
 
-	{ MODKEY,               XK_udiaeresis, killclient,  { 0 } },
+	{ ControlMask,           XK_w,          killclient,  { 0 } },
 };
