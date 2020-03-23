778ecc86d7a22dceb4c580b8a29d845b59a4bb2e
Markus Teich <markus.teich@stusta.mhn.de>
2020-03-23 04:19:24 +0100

    add ctrl-t mapping

--- config.h
+++ config.h
@@ -40,21 +40,20 @@ static Key keys[] = {
 	/* modifier              key            function     argument */
 	{ MODKEY,                XK_z,          focusonce,   { 0 } },
 	{ MODKEY,                XK_z,          spawn,       { 0 } },
-
 	{ MODKEY,                XK_adiaeresis, rotate,      { .i = +1 } },
 	{ MODKEY,                XK_odiaeresis, rotate,      { .i = -1 } },
 	{ MODKEY|ShiftMask,      XK_odiaeresis, movetab,     { .i = -1 } },
 	{ MODKEY|ShiftMask,      XK_adiaeresis, movetab,     { .i = +1 } },
+	{ MODKEY,                XK_udiaeresis, killclient,  { 0 } },
 
+	{ ControlMask,           XK_t,          focusonce,   { 0 } },
+	{ ControlMask,           XK_t,          spawn,       { 0 } },
 	{ ControlMask,           XK_Tab,        rotate,      { .i = +1 } },
 	{ ControlMask|ShiftMask, XK_Tab,        rotate,      { .i = -1 } },
 	{ ControlMask|ShiftMask, XK_Page_Up,    movetab,     { .i = -1 } },
 	{ ControlMask|ShiftMask, XK_Page_Down,  movetab,     { .i = +1 } },
+	{ ControlMask,           XK_F4,         killclient,  { 0 } },
 
 	{ MODKEY,                XK_f,          rotate,      { .i = 0 } },
-
 	{ MODKEY,                XK_p,          spawn,       SETPROP("_TABBED_SELECT_TAB") },
-
-	{ MODKEY,                XK_udiaeresis, killclient,  { 0 } },
-	{ ControlMask,           XK_F4,         killclient,  { 0 } },
 };
